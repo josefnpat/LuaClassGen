d3a72bac6efbd80c85953cde8773393714145bfd
Seppi <seppi@josefnpat.com>
2015-01-13 21:44:28 -0500

    Reorging tests

---
+++ run-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+LUA="lua5.1"
+
+# Generate classes
+echo "Generating Classes:"
+./classgen -i tests/test-variables.lua.cg
+./classgen -i tests/test-speed.lua.fast.cg
+./classgen -i tests/test-speed.lua.slow.cg
+./classgen -i tests/test-collections.lua.cg
+
+# Run tests
+echo "Variable Test:"
+$LUA tests/test-variables.lua
+echo "Speed Test:"
+$LUA tests/test-speed.lua
+echo "Collections Test:"
+$LUA tests/test-collections.lua
